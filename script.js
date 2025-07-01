710a29d4af81a703b2b6bd9fe70de27b65179063
Arthur <arthurprisso@gmail.com>
2025-07-01 00:34:00 -0300

    2

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-var key = "Monotropa uniflora"
-var input
+var sigma;
 
-document.getElementById("Submit").onclick = function(){
-    input = document.getElementById("Input").value
-    document.getElementById("h21").textContent = ''
-    document.getElementById("h22").textContent = ''
-    if(key = input){
-        document.getElementById("h1").textContent = "Dvd... aopz tlhuz h sva av tl..."
-        document.getElementById("h21").textContent = "fvb hyl aol vusf wlyzvu p jhu ahsr av, fvb ruvd?"
-        document.getElementById("h22").textContent = "fvb hyl aol vusf vul dov buklyzahukz tl..."
-    }
-    else{
-        document.getElementById("h1").textContent = "Aopz pz ovyypisl."
-        document.getElementById("h21").textContent = "Dof dvbsk fvb npcl tl aopz."
-    }
-}
+document.getElementById("Submit").onclick = function () {
+  sigma = document.getElementById("Input").value;
+  console.log(sigma)
+  document.getElementById("h21").textContent = "";
+  document.getElementById("h22").textContent = "";
+  if ((sigma === "Monotropa uniflora")) {
+    document.getElementById("h1").textContent =
+      "Dvd... aopz tlhuz h sva av tl...";
+    document.getElementById("h21").textContent =
+      "fvb hyl aol vusf wlyzvu p jhu ahsr av, fvb ruvd?";
+    document.getElementById("h22").textContent =
+      "fvb hyl aol vusf vul dov buklyzahukz tl...";
+  } else {
+    document.getElementById("h1").textContent = "Aopz pz ovyypisl.";
+    document.getElementById("h21").textContent = "Dof dvbsk fvb npcl tl aopz.";
+  }
+};
